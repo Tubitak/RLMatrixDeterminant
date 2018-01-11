966dcdecc8bdbcd2ca6816d85657726814eea515
Tubitak <35142933+Tubitak@users.noreply.github.com>
2018-01-11 22:04:00 +0100

    Add files via upload
    Added discrete version of action space and testing for different agents

---
+++ agent_learning.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+from tensorforce.agents.vpg_agent import VPGAgent
+from tensorforce.agents.trpo_agent import TRPOAgent
+from tensorforce.agents.ppo_agent import PPOAgent
+from tensorforce.agents.dqn_agent import DQNAgent
+from tensorforce.agents.ddqn_agent import DDQNAgent
+from tensorforce.agents.dqn_nstep_agent import DQNNstepAgent
+from tensorforce.agents.dqfd_agent import DQFDAgent
+from tensorforce.execution import Runner
+from tensorforce.contrib.openai_gym import OpenAIGym
+import gym_det_env
+
+# Create an OpenAIgym environment
+env = OpenAIGym('GymDet-v0')
+
+'''
+{
+    "type": "embedding",
+    "indices": 100,
+    "size": 32
+},
+'''
+
+# Network as list of layers
+network_spec = [
+    {
+        "type": "dense",
+        "size": 256,
+        'activation': 'tanh'
+    },
+
+    {
+        "type": "dense",
+        "size": 128,
+        'activation': 'tanh'
+    },
+
+    {
+        "type": "dense",
+        "size": 64,
+        'activation': 'tanh'
+    }
+]
+
+agents = dict(
+    vpg_agent=VPGAgent,
+    trpo_agent=TRPOAgent,
+    ppo_agent=PPOAgent,
+    dqn_agent=DQNAgent,
+    ddqn_agent=DDQNAgent,
+    dqn_nstep_agent=DQNNstepAgent,
+    dqfd_agent=DQFDAgent
+)
+
+scores = {}
+
+for agent_class in agents.values():
+    print(agent_class)
+    env = OpenAIGym('GymDet-v0')
+    agent = agent_class(states_spec=env.states, actions_spec=env.actions, network_spec=network_spec)
+
+    runner = Runner(agent=agent, environment=env)
+
+
+    def episode_finished(r):
+        print(
+            "Finished episode {ep} after {ts} timesteps (reward: {reward})".format(ep=r.episode, ts=r.episode_timestep,
+                                                                                   reward=r.episode_rewards[-1]))
+        return True
+
+
+    # Start learning
+    runner.run(episodes=10000, max_episode_timesteps=200, episode_finished=None)
+
+    # Print statistics
+    print("Learning finished. Total episodes: {ep}. Average reward of last 100 episodes: {ar}.".format(
+        ep=runner.episode,
+        ar=np.mean(runner.episode_rewards[-100:]))
+    )
+
+    scores[agent_class] = np.mean(runner.episode_rewards[-100:])
+
+for agent, score in scores.items():
+    print(agent, score)
--- gym_det_env/env/gym_det.py
+++ gym_det_env/env/gym_det.py
@@ -9,26 +9,27 @@ class Determinant_v0(gym.Env):
     The goal of Determinant is to find a non-invertible matrix
 
     After each step the agent receives an observation of:
-    r - determinant of matrix
+    m - matrix
 
     The rewards is calculated as:
     (det(new_matrix) - det(old_matrix)) ** 2
 
-    TODO: Vidi li celu matricu? Zasad izgleda da ne. Takodje, popraviti sve kao u v1. Za sad nije dobro.
+    TODO: Implementirati da gadja tacno target_determinant
     """
+
     def __init__(self):
 
-        self.target_determinant = 0.0
+        self.target_determinant = 1.0
 
-        self.n = 6
+        self.n = 2
 
-        self.action_space = spaces.Box(low=np.array([0]), high=np.array([self.n**2]))
-        self.observation_space = spaces.Box(low=np.array([-100]), high=np.array([100]))
+        self.action_space = spaces.Discrete(self.n ** 2)
+        self.observation_space = spaces.Box(low=0., high=1., shape=(self.n * self.n))
 
         self.matrix = np.random.randint(0, 2, [self.n, self.n]).astype(np.float32)
         self.guess_count = 0
         self.guess_max = 200
-        self.observation = [np.linalg.det(self.matrix)]
+        self.observation = self.matrix.reshape(self.n ** 2)  # A view of self.matrix, not a new array
 
         self._seed()
         self._reset()
@@ -38,32 +39,43 @@ class Determinant_v0(gym.Env):
         return [seed]
 
     def _step(self, action):
+        # TODO: srediti ove crne oblike
+
         assert self.action_space.contains(action)
 
-        action = int(action[0])  # korektno?
+        value_on_action = self.matrix.reshape(self.n ** 2)[action]
+        old_matrix = np.array(self.matrix)
 
-        value_on_action = self.matrix.reshape(self.n**2)[action]
         if value_on_action == 0.:
-            new_matrix = self.matrix.reshape(self.n**2)
+            new_matrix = self.matrix.reshape(self.n ** 2)
             new_matrix[action] = 1.
             self.matrix = new_matrix.reshape([self.n, self.n])
         if value_on_action == 1.:
-            new_matrix = self.matrix.reshape(self.n**2)
+            new_matrix = self.matrix.reshape(self.n ** 2)
             new_matrix[action] = 0.
             self.matrix = new_matrix.reshape([self.n, self.n])
 
-        reward = - (self.observation[0] - np.linalg.det(self.matrix)) ** 2
-        self.observation = [np.linalg.det(self.matrix)]
+        EPSILON = 0.0001
+        reward = 0
+        if (self.target_determinant - np.linalg.det(self.matrix)) ** 2 >= EPSILON:
+            reward = -1
+        if (self.target_determinant - np.linalg.det(self.matrix)) ** 2 < EPSILON:
+            reward = 1
+
+        self.observation = self.matrix.reshape(self.n ** 2)
 
         self.guess_count += 1
-        done = self.guess_count >= self.guess_max
+        done = self.guess_count >= self.guess_max or reward > 0
+
+        # if done:
+        #    print(self.matrix, np.linalg.det(self.matrix))
 
         return self.observation, reward, done, {"matrix": self.matrix, "guesses": self.guess_count}
 
     def _reset(self):
         self.matrix = np.random.randint(0, 2, [self.n, self.n]).astype(np.float32)
         self.guess_count = 0
-        self.observation = [1.]
+        self.observation = self.matrix.reshape(self.n ** 2)
         return self.observation
 
 
@@ -84,7 +96,7 @@ class Determinant_v1(gym.Env):
 
             self.target_determinant = 1.0
 
-            self.n = 6
+            self.n = 15
 
             self.action_space = spaces.Box(low=np.array([0]), high=np.array([self.n ** 2]))
             self.observation_space = spaces.Box(low=0., high=1., shape=(self.n * self.n))
@@ -105,7 +117,6 @@ class Determinant_v1(gym.Env):
             # TODO: srediti ove crne oblike
 
             assert self.action_space.contains(action)
-
             action = int(action[0])  # korektno?
 
             value_on_action = self.matrix.reshape(self.n ** 2)[action]
